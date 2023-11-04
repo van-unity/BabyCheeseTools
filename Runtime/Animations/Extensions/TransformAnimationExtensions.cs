c762dfb68b208709c65800e64ea748169eca0110
van <vanik.121290@gmail.com>
2023-11-04 22:00:29 +0400

    add oncomplete callback

--- Runtime/Animations/Extensions/TransformAnimationExtensions.cs
+++ Runtime/Animations/Extensions/TransformAnimationExtensions.cs
@@ -4,31 +4,60 @@ using UnityEngine;
 
 namespace Animations.Extensions {
     public static class TransformAnimationExtensions {
-        public static IEnumerator Move(this Transform transform, Vector3 to, float duration, Easing easing) {
+
+        public static IEnumerator Move(this Transform transform, Vector3 to, float duration, Easing easing, Action onComplete = null) {
             var currentPos = transform.position;
-            return Iterator(duration, easing, value => { transform.position = Vector3.Lerp(currentPos, to, value); });
+            return Iterator(duration, easing, value => transform.position = Vector3.Lerp(currentPos, to, value), onComplete);
         }
 
-        public static IEnumerator MoveLocal(this Transform transform, Vector3 to, float duration, Easing easing) {
+        public static IEnumerator MoveLocal(this Transform transform, Vector3 to, float duration, Easing easing, Action onComplete = null) {
             var currentPos = transform.localPosition;
-            return Iterator(duration, easing,
-                value => { transform.localPosition = Vector3.Lerp(currentPos, to, value); });
+            return Iterator(duration, easing, value => transform.localPosition = Vector3.Lerp(currentPos, to, value), onComplete);
         }
 
-        public static IEnumerator RotateEuler(this Transform transform, Vector3 to, float duration, Easing easing,
-            bool fastBeyond360 = false) {
+        public static IEnumerator RotateEuler(this Transform transform, Vector3 to, float duration, Easing easing, bool fastBeyond360 = false, Action onComplete = null) {
             var currentEuler = transform.eulerAngles;
             Vector3 toEuler = fastBeyond360 ? to : OptimizeEuler(to, currentEuler);
-            return Iterator(duration, easing,
-                value => { transform.eulerAngles = Vector3.Lerp(currentEuler, toEuler, value); });
+            return Iterator(duration, easing, value => transform.eulerAngles = Vector3.Lerp(currentEuler, toEuler, value), onComplete);
         }
 
-        public static IEnumerator RotateEulerLocal(this Transform transform, Vector3 to, float duration, Easing easing,
-            bool fastBeyond360 = false) {
+        public static IEnumerator RotateEulerLocal(this Transform transform, Vector3 to, float duration, Easing easing, bool fastBeyond360 = false, Action onComplete = null) {
             var currentEuler = transform.localEulerAngles;
             Vector3 toEuler = fastBeyond360 ? to : OptimizeEuler(to, currentEuler);
-            return Iterator(duration, easing,
-                value => { transform.localEulerAngles = Vector3.Lerp(currentEuler, toEuler, value); });
+            return Iterator(duration, easing, value => transform.localEulerAngles = Vector3.Lerp(currentEuler, toEuler, value), onComplete);
+        }
+
+        public static IEnumerator RotateQuaternion(this Transform transform, Quaternion to, float duration, Easing easing, Action onComplete = null) {
+            var currentRot = transform.rotation;
+            return Iterator(duration, easing, value => transform.rotation = Quaternion.Lerp(currentRot, to, value), onComplete);
+        }
+
+        public static IEnumerator RotateQuaternionLocal(this Transform transform, Quaternion to, float duration, Easing easing, Action onComplete = null) {
+            var currentRot = transform.localRotation;
+            return Iterator(duration, easing, value => transform.localRotation = Quaternion.Lerp(currentRot, to, value), onComplete);
+        }
+
+        public static IEnumerator Scale(this Transform transform, Vector3 to, float duration, Easing easing, Action onComplete = null) {
+            var currentScale = transform.localScale;
+            return Iterator(duration, easing, value => transform.localScale = Vector3.Lerp(currentScale, to, value), onComplete);
+        }
+
+        private static IEnumerator Iterator(float duration, Easing easing, Action<float> callback, Action onComplete) {
+            float time = 0;
+            var easingFunction = EasingFunctions.FunctionByEasing[easing];
+
+            while (time < duration) {
+                time += Time.deltaTime;
+                var t = easingFunction(time / duration);
+                callback.Invoke(t);
+                yield return null;
+            }
+
+            // Ensure the final state is set
+            callback.Invoke(1);
+
+            // Call the onComplete action if it's not null
+            onComplete?.Invoke();
         }
 
         // Helper method to find the shortest path for rotation if not using FastBeyond360 mode
@@ -38,48 +67,12 @@ namespace Animations.Extensions {
                 if (Mathf.Abs(targetEuler[i] - currentEuler[i]) > 180f) {
                     if (targetEuler[i] > currentEuler[i]) {
                         optimizedEuler[i] -= 360f;
-                    }
-                    else {
+                    } else {
                         optimizedEuler[i] += 360f;
                     }
                 }
             }
-
             return optimizedEuler;
-        }
-
-        public static IEnumerator RotateQuaternion(this Transform transform, Quaternion to, float duration,
-            Easing easing) {
-            var currentRot = transform.rotation;
-            return Iterator(duration, easing,
-                value => { transform.rotation = Quaternion.Lerp(currentRot, to, value); });
-        }
-
-        public static IEnumerator RotateQuaternionLocal(this Transform transform, Quaternion to, float duration,
-            Easing easing) {
-            var currentRot = transform.localRotation;
-            return Iterator(duration, easing,
-                value => { transform.localRotation = Quaternion.Lerp(currentRot, to, value); });
-        }
-
-        public static IEnumerator Scale(this Transform transform, Vector3 to, float duration, Easing easing) {
-            var currentScale = transform.localScale;
-            return Iterator(duration, easing,
-                value => { transform.localScale = Vector3.Lerp(currentScale, to, value); });
-        }
-
-        private static IEnumerator Iterator(float duration, Easing easing, Action<float> callback) {
-            float time = 0;
-            var easingFunction = EasingFunctions.FunctionByEasing[easing];
-
-            while (time <= duration) {
-                time += Time.deltaTime;
-                var t = easingFunction(time / duration);
-                callback.Invoke(t);
-                yield return null;
-            }
-
-            callback?.Invoke(1);
         }
     }
 }
