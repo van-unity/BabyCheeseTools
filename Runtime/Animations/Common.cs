5c5248216b2f27dde3dc9416535f6aafb13b4d03
van <vanik.121290@gmail.com>
2023-11-06 20:27:19 +0400

    add material color animation

---
+++ Runtime/Animations/Common.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Animations {
+    public static class Common {
+        public static IEnumerator Iterator(float duration, Easing easing, Action<float> callback, Action onComplete) {
+            float time = 0;
+            var easingFunction = EasingFunctions.FunctionByEasing[easing];
+
+            while (time < duration) {
+                time += Time.deltaTime;
+                var t = easingFunction(time / duration);
+                callback.Invoke(t);
+                yield return null;
+            }
+
+            // Ensure the final state is set
+            callback.Invoke(1);
+
+            // Call the onComplete action if it's not null
+            onComplete?.Invoke();
+        }
+    }
+}
---
+++ Runtime/Animations/Extensions/MaterialAnimationExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Animations.Extensions {
+    public static class MaterialAnimationExtensions {
+        public static IEnumerator AnimateColor(this Material material, Color to, float duration, Easing easing,
+            Action onComplete = null) {
+            var startColor = material.color;
+            return Common.Iterator(duration, easing,
+                value => material.color = Color.Lerp(startColor, to, value), onComplete);
+        }
+    }
+}
