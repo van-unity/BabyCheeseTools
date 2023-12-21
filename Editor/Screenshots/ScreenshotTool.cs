099cb5ae8068d8ef7a92078cbbc9f55b5c67ea68
van <vanik.121290@gmail.com>
2023-12-21 22:10:45 +0400

    remember last path

--- Editor/Screenshots/ScreenshotTool.cs
+++ Editor/Screenshots/ScreenshotTool.cs
@@ -1,16 +1,21 @@
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
 namespace BabyCheeseTools.Editor.Screenshots {
     public static class ScreenshotTool {
+        private static string _lastDirectoryPath;
+        
         [MenuItem("BabyCheese/Tools/Take Screenshot")]
         private static void TakeScreenshot() {
             string path = EditorUtility.SaveFilePanel(
                 "Save screenshot",
-                "",
+                _lastDirectoryPath,
                 "Screenshot.png",
                 "png");
 
+            _lastDirectoryPath = Path.GetDirectoryName(path);
+            
             if (path.Length != 0) {
                 ScreenCapture.CaptureScreenshot(path);
                 // Optional: Open the folder containing the screenshot
